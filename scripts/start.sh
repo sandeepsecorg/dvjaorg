94d7e274b4bc6d5b414f4fc386c757bcec01be70
sandeepvm17 <51812132+sandeepvm17@users.noreply.github.com>
2024-02-08 11:38:05 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Wait for DB container to initialize
-sleep 10
+sleep 15
 
 mysql -h mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "CREATE DATABASE dvja CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
 mysql -h mysql -u $MYSQL_USER -p$MYSQL_PASSWORD dvja < ./db/schema.sql
